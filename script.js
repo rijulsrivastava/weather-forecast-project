955dd76209fb9934b559ced6f939b539807e3c25
rijulsrivastava <rijulsrivastva@gmail.com>
2026-02-18 16:10:10 +0530

    Gps location functionality has been added

--- script.js
+++ script.js
@@ -8,3 +8,15 @@ searchBtn.addEventListener("click", ()=>{
     }
     console.log("Entered city:" + city.value)
 });
+
+const locationBtn = document.querySelector("#location");
+
+locationBtn.addEventListener("click", () => {
+    navigator.geolocation.getCurrentPosition(geoPosition => {
+        const lat = geoPosition.coords.latitude;
+        const long = geoPosition.coords.longitude;
+        console.log("latitude:" + lat , "longitude:" + long)
+    }, (e) => {
+        console.log("Can not get location",e)
+    });
+});
