024e43caa2ef598e0bd409afaf88dd30f01cafb4
rijulsrivastava <rijulsrivastva@gmail.com>
2026-02-18 18:50:04 +0530

    Current weather forcast feature has been added

--- script.js
+++ script.js
@@ -30,7 +30,7 @@ function forecastByCity(city) {
             return resolution.json();
         })
         .then(data => {
-            console.log("weather data:",data)
+            displayWeather(data)
         })
         .catch(error => {
             console.log(error);
@@ -41,6 +41,27 @@ function forecastByCoordinates(lat, long) {
     fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&units=metric&appid=${"e07e1f450087e2c0e4e9d3694e0dec43"}`)
         .then(resolution => resolution.json())
         .then(data => {
-            console.log("weather data:",data)
+            displayWeather(data)
         });
 }
+
+const currentWeather = document.querySelector("#currentWeather");
+const cityName = document.querySelector("#cityName");
+const temperature = document.querySelector("#temperature");
+const weatherImage = document.querySelector("#weatherImage");
+const description = document.querySelector("#description");
+const wind = document.querySelector("#wind");
+const humidity = document.querySelector("#humidity");
+
+function displayWeather(data) {
+    currentWeather.classList.remove("hidden");
+    cityName.textContent = data.name;
+    currentTemperature = data.main.temp;
+    temperature.textContent = currentTemperature.toFixed(1) + "°C";
+    weatherImage.src = `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
+    description.textContent = data.weather[0].description;
+    wind.textContent = `Wind Speed: ${data.wind.speed} m/s`;
+    humidity.textContent = `Humidity: ${data.main.humidity}%`;
+}
+
+
